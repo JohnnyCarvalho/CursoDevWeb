898cd7a7d1222ca380d370455615d68460e7bc32
JohnnyCarvalho <johnnycarvalhoem@gmail.com>
2020-07-12 00:42:13 -0300

    at

--- EXERCICIO-JS/algunsCuidadosComNumeros.js
+++ EXERCICIO-JS/algunsCuidadosComNumeros.js
@@ -1,16 +1,16 @@
-/** algumas operaçõe em Java Script são meio estranhas, vejamos alguma delas */
+/** algumas operações em Java Script são meio estranhas, vejamos algumas */
 
 /**
- * O exemplo abaixo em outras linguagens de programação isso causaria um erro, mais em Java não, 
+ * O exemplo abaixo, em outras linguagens de programação causaria um erro, mais em JS não, 
  * essa função metemática vai imprimir infinity no terminal pois em JS qualquer valor dividido por 0
  * gera um numero infinito
  */
 console.log(7/0); 
 
 /**
- * No exemplo abaixo em outras linguagens jamais daria certo uma coisa dessas,
- *  em JS como se trata de uma linguagem fracamente tipada, 
- * ela divide esse valor 10 que está em forma de string por 2 e o resultado no terminal é 5
+ * No exemplo abaixo em outras linguagens jamais daria certo, em JS
+ * como se trata de uma linguagem fracamente tipada, 
+ * ela divide esse valor (10) que está em forma de string por (2) ou seja (10/2) e o resultado no terminal é 5
  */
 console.log('10'/2);
 
@@ -21,7 +21,7 @@ console.log('10'/2);
 console.log('Johnny'*2);
 
 /**
- * Para manipularmos as casas decimais não podemos atacar diretamente o número com o toString por exemplo
+ * Para manipularmos as casas decimais podemos atacar diretamente o número com o toString ou não
  * vejamos nos exemplos abaixo
  */
 console.log(10.7458.toFixed(2)); //esse modo está errado
